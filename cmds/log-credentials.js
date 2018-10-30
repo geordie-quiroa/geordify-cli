61b7e92b7e728154e338fcddd3f297b5fe93ae78
geordie-quiroa <gqmail@protonmail.com>
2018-10-29 19:22:55 -0600

    clientID, token

--- cmds/log-credentials.js
+++ cmds/log-credentials.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    spotifyClientId:'38e377baa81747a68c4cb3677646cc16',
-    spotifySecretToken:'87c0344055ab4e7cabc6924c12e245bf'
+    spotifyClientId:'your-client-id',
+    spotifySecretToken:'your-secret-token'
 }
